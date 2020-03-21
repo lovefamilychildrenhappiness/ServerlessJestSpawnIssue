4903dccffcc729ffb794ef6dbe618ed3b1bd7e05
viggy <dviglione02@gmail.com>
2020-03-21 10:56:32 -0400

    integrate tests

--- hooks.js
+++ hooks.js
@@ -1,16 +1,26 @@
 const aws = require('aws-sdk');
 const codedeploy = new aws.CodeDeploy({apiVersion: '2014-10-06'});
+import * as path from 'path';
+import { runCLI } from 'jest';
 
 module.exports.pre = (event, context, callback) => {
   var deploymentId = event.DeploymentId;
   var lifecycleEventHookExecutionId = event.LifecycleEventHookExecutionId;
 
-  console.log('We are running some integration tests before we start shifting traffic...');
+  const resp = await runCLI({ testMatch: ['<rootDir>/spec/integration/hello/*.test.js'] }, ['/var/task']);
+
+  const result = resp.results.testResults.filter( (result) => path.basename(result.testFilePath) === 'index.test.js' );
+
+  const success = result.every( test => test.numFailingTests === 0);
+
+  const validationTestResult = success === true ? 'Succeeded' : 'Failed';
+
+  console.log('the validationTestResult: ', validationTestResult)
 
   var params = {
       deploymentId: deploymentId,
       lifecycleEventHookExecutionId: lifecycleEventHookExecutionId,
-      status: 'Succeeded' // status can be 'Succeeded' or 'Failed'
+      status: validationTestResult // status can be 'Succeeded' or 'Failed'
   };
 
   return codedeploy.putLifecycleEventHookExecutionStatus(params).promise()
---
+++ spec/integration/hello/index.test.js
@@ -0,0 +1,5 @@
+describe('Example', () => {
+    it('returns 2', async () => {
+        expect(1 + 1).toBe(2);
+    });
+})
